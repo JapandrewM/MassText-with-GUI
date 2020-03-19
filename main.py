8f9120cb917abb917643eeb79a3bee132228fb99
Andrew M <62349498+JapandrewM@users.noreply.github.com>
2020-03-18 20:35:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 import csv
 import os
-import PyQt5
+from twilio.rest import Client #TwilioRestClient
+import openpyxl
 
-def main():
-    pass
+AUTH_SID = '##########' # Twilio SID
+TEST_SID = '#########' # 
 
-if __name__ == "__main__":
+AUTH_TOKEN = '###########' # Twilio Token, --take out of public repo--
+TEST_TOKEN = '###########' # 
+
+test_mode = True
+
+if test_mode == False:
+    token = AUTH_TOKEN
+    sid = AUTH_SID
+    client = Client(sid, token)#TwilioRestClient(sid, token)
+else:
+    token = TEST_TOKEN
+    sid = TEST_SID
+    #client = Client(sid, token)#TwilioRestClient(sid, token)
+
+
+
+wb = openpyxl.load_workbook("CTS_Employee_Numbers.xlsx")
+shtNames = wb.sheetnames()
+sheet = wb['Test']
+col = sheet['C']
+rowObj =tuple(sheet['A:C'])
+
+#how to go row by row, so I can access names with appropriate numbers?
+
+for cell in col:
+    if test_mode == True:
+        print(shtNames)
+    elif test_mode == False:
+        if cell.value:
+            client.messages.create(
+                to = cell.value,
+                from_ = '+12057231125', # put your own Twilio phone number here
+                body = 'This is a Crown Technical Systems emergency text test')
+
+
+#Not sure where to put this
+#import PyQt5
+##def main():
+    #pass
+
+#if __name__ == "__main__":
     #execute program
-    main()
+    #main()
